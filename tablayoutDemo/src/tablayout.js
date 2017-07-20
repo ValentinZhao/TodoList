52f0465a4bf5dafbf31657c3de87603b61b327e2
ValentinZhao <2264709714@qq.com>
2017-07-20 17:26:06 +0800

    node note

--- tablayoutDemo/src/tablayout.js
+++ tablayoutDemo/src/tablayout.js
@@ -1,3 +1,14 @@
 $(function(){
-	
+	var swipeWatcher = function(){
+		var h_swipeHeader = new Hammer($('#head')[0]);
+		var swipeHeader = $('#head');
+		var itemNo = 0;
+		return function(i){
+			h_swipeHeader.on('pan', function(){
+				swipeHeader.css('marginLeft', '1rem');
+				itemNo = i;
+			});
+		}
+	}
+	swipeWatcher();
 });
